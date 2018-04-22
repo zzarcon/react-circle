7ca19f7a2b705b461826cd5527b6b5d122529547
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-04-22 17:19:25 +1000

    Add github link to example

--- example/styled.ts
+++ example/styled.ts
@@ -8,6 +8,12 @@ injectGlobal`
   body {
     background: linear-gradient(90deg, #69b7eb, #b3dbd3, #f4d6db);
   }
+
+  .github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}
+
+  .github-corner svg {
+    fill:#64CEAA; color:#fff; position: absolute; top: 0; border: 0; right: 0;
+  }
 `;
 
 export const AppWrapper = styled.div`
